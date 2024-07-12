aa58c753bf7babf6cafdfab9c0674e7dda918496
junnun15 <junnunprodhan@gmail.com>
2024-07-12 21:55:44 +0600

    implement searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-
+import './SearchBar.css';
 const SearchBar = ({ players, setFilteredPlayers }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -16,22 +16,24 @@ const SearchBar = ({ players, setFilteredPlayers }) => {
   };
 
   return (
-    <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search by player name..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <motion.button
-        className="search-button"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        onClick={() => console.log('Search clicked')}
-      >
-        Search
-      </motion.button>
-    </div>
+    <div className="search-container w-full md:w-3/4 mx-auto flex items-center justify-center">
+  <input
+    className="w-full py-2 px-4 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
+    type="text"
+    placeholder="Search by player name..."
+    value={searchTerm}
+    onChange={handleSearch}
+  />
+
+  <motion.button
+    className="search-button ml-2"
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+    onClick={() => console.log('Search clicked')}
+  >
+    Search
+  </motion.button>
+</div>
   );
 };
 
